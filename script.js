33e74d6eb693961463d3c8014bc202d840cf6433
Naushi-Ali <49521116+naushi-Ali@users.noreply.github.com>
2019-07-11 16:09:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let techkaro = "Tech karo"
+
+courseName: "Tech karo"
+students:
+[
+        name:
+            profession:
+        hobbies:
+            contact:
+        dateOfBirth:
+            address:
+        email:
+            qualification:
+        gender:
+            rollNo:
+        group:
+            fatherName:
+        fatherOccupation:
+            MatitalStatus:
+        fatherIncome:
+            PersonalIncome:
+        siblings:
+            spouseOccupation:
+        referance:
+            extraSkills:
+        CNIC:
+            isEmployed:
+        EmploymentInfo: {
+            designation: "accountant",
+            "meezan bank",
+            300000,
+            "9 AM-5 AM",
+            distance: 10
+            WillComeDirectlrFrom: true
+
+        },
+
+
+    }
+    }
+
+],
+    sponser: [
+
+
+    ],
+        batchNo: 2019,
+            faculty:
+ambessedors: [
+
+    {
+        name:
+            qualification:
+        profilePic:
+    },
+],
+    Duration:
+founder:
+fees:
+expenses
+classes
+location
+mentors
+lifeskill:
+syllabus:
+title:
+purpose:
+Schedule:
+exams:
+projects:
+events:
+successStories:
